9cea9f6b59748dbff5a1a3f28c1bd962d9b5ce39
felipe300 <caracoloro@gmail.com>
2024-08-10 18:50:39 -0400

    feat: handling put request

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import createHomepageTemplate from "./views/index.js";
 import listOfBooks from "./views/list.js";
 import createBookTemplate from "./views/book.js";
 import BOOKS_DATA from "./data/data.js";
+import createEditFormTemplate from "./views/edit.js";
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -22,11 +23,26 @@ app.get("/books/:id", (req, res) => {
   res.send(createBookTemplate(book));
 });
 
+app.get("/books/edit/:id", (req, res) => {
+  const { id } = req.params;
+  const book = BOOKS_DATA.find((b) => b.id === id);
+  res.send(createEditFormTemplate(book));
+});
+
 app.post("/books", (req, res) => {
   const { title, author } = req.body;
   const id = Math.random().toString();
   BOOKS_DATA.push({ id, title, author });
   res.redirect(`/books/${id}`);
+});
+
+app.put("/books/:id", (req, res) => {
+  const { id } = req.params;
+  const { author, title } = req.body;
+  const newBook = { author, id, title };
+  const idx = BOOKS_DATA.findIndex((b) => b.id === id);
+  BOOKS_DATA[idx] = newBook;
+  res.send(createBookTemplate(newBook));
 });
 
 app.delete("/books/:id", (req, res) => {
